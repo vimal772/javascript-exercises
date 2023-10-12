1b25dfcd14596e9be0a62ab6770f086a0224868e
vimal772 <vimal115077286@gmail.com>
2023-10-12 11:21:14 +0530

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson,currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth,oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
 
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
+
+function getAge(birthYear,deathYear){
+    if(!deathYear){
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
