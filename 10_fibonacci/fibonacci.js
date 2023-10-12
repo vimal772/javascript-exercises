7a7865d470ebae3406beab1015612f90d6b54ffe
vimal772 <vimal115077286@gmail.com>
2023-10-12 10:25:01 +0530

    fibonacci excercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n<0){
+        return "OOPS";
+    }else{
+        let num1 = 1;
+        let num2 = 1;
+        let num3;
+        if(n===1 || n===2){
+            return num1;
+        }
+        for(let i=3;i<=n;i++){
+            num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+        }
+        return num3;
+    }
 };
 
 // Do not edit below this line
